b132b113e4d1bcf84284388de85dfe6be871ba6b
sodp5 <sodp5@naver.com>
2020-03-02 12:52:19 +0900

    프로그래머스 해시 2단계 전화번호 해결(수정)

--- src/Programmers_hash_2_phonebook.java
+++ src/Programmers_hash_2_phonebook.java
@@ -1,18 +1,9 @@
-import java.util.Arrays;
-import java.util.Comparator;
 import java.util.HashMap;
 
 public class Programmers_hash_2_phonebook {
     public boolean solution(String[] phone_book) {
         boolean answer = true;
         HashMap<String, Boolean> map = new HashMap<>();
-
-        Arrays.sort(phone_book, new Comparator<String>() {
-            @Override
-            public int compare(String o1, String o2) {
-                return o1.length() - o2.length();
-            }
-        });
 
         for (int i = 0; i < phone_book.length; i++) {
             map.put(phone_book[i], true);
